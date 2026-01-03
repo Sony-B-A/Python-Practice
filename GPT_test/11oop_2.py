0037df839bbe8b0e2aab70cba002ea9dab5f5469
Sony B A <sonyshaiva444@gmail.com>
2026-01-03 22:47:52 +0530

    moved the folder so now committing againg all it's really exhausting

---
+++ GPT_test/11oop_2.py
@@ -0,0 +1,16 @@
+'''Write a function that:Takes a sentence Returns:
+number of characters (excluding spaces) number of vowels'''
+
+'''def val_counts(s):
+    vow_count = 0
+    sent_list = s.split()
+    for ch in s:
+        if ch in "aeiou":
+            vow_count += 1
+    return vow_count, len(sent_list)
+
+sentence = input("Enter a sentence: ")
+v, l = val_counts(sentence.lower())
+print(f"the sentence has {l} words and {v} vowels")'''
+
+
