3b1495b9f32d985cf4eaf1d4189ada573992e8e5
Sony B A <sonyshaiva444@gmail.com>
2026-01-07 22:27:47 +0530

    showed up little bit

---
+++ GPT_test/13.py
@@ -0,0 +1,18 @@
+# 🧪 TEST 2 — Dictionary + Function + Logic
+marks = {
+    "Sony": 78,
+    "Preetham": 85,
+    "Sangeeta": 88,
+    "Aish": 92
+}
+ 
+def fxn(data):
+    high_marks = max(data.values())
+    for key in data:
+        if(data.get(key) == high_marks):
+            name = key
+    return key, high_marks
+
+name, score = fxn(marks)
+tup = (name, score)
+print(tup)
